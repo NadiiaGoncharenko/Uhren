2c24b0a54af67e9b1ce90545bd596eebcb75b710
salmarashwan <118923981+salmarashwan@users.noreply.github.com>
2022-12-21 13:28:24 +0100

    Update dbaccess.php

--- dbaccess.php
+++ dbaccess.php
@@ -1,8 +1,8 @@
 <?php
  //verbindung zur db herstellen
     $host = "localhost";
-    $user = "teamsnooze"; 
-    $password = "teamsnooze";
-    $database = "snooze";
+    $user = "watchsearch"; 
+    $password = "watchsearch";
+    $database = "watchsearch";
 
 ?>
