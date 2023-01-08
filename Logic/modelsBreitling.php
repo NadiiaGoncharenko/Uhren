1e746aab11902bbce9d152d7393c5bd7936edae4
Nadiia Goncharenko <83373973+NadiiaGoncharenko@users.noreply.github.com>
2023-01-08 22:11:59 +0100

    Add files via upload

---
+++ Logic/modelsBreitling.php
@@ -0,0 +1,275 @@
+<?php
+
+function  modelBreitling($model){
+
+    switch ($model) {
+        case 'Aerospace':
+            $model = '&models=24';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+
+        case '':
+            $model = '&models=';
+            break; '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+
+        case '':
+            $model = '&models=';
+            break;case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+
+        case '':
+            $model = '&models=';
+            break;case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+
+        case '':
+            $model = '&models=';
+            break;case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+
+        case '':
+            $model = '&models=';
+            break;case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+
+        case '':
+            $model = '&models=';
+            break;case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+
+        case '':
+            $model = '&models=';
+            break;case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+
+        case '':
+            $model = '&models=';
+            break;case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+
+        case '':
+            $model = '&models=';
+            break;case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+
+        case '':
+            $model = '&models=';
+            break;case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+        case '':
+            $model = '&models=';
+            break;
+
+        case '':
+            $model = '&models=';
+            break;
+
+}
+return $model;
+}
+?>
---
+++ Logic/modelsPP.php
@@ -0,0 +1,47 @@
+<?php
+
+function  modelPP($model){
+switch ($model) {
+
+    case 'Annual Calendar':
+        $model = '&models=1963';
+        break;
+        
+    case 'Aquanaut':  $model = '&models=92';
+        break;
+        case 'Beta 21':  $model = '&models=2013';
+        break;
+        case 'Calatrava':  $model = '&models=93';
+        break;
+        case 'Chronograph':  $model = '&models=1964';
+        break;
+        case 'Complications':  $model = '&models=96';
+        break;
+        case 'Ellipse dOr':  $model = '&models=97'; //Name is Ellipse d'Or for front
+        break;
+        case 'Gondolo':  $model = '&models=98';
+        break;
+        case 'Hour Glass':  $model = '&models=102';
+        break;
+        case 'Minute Repeater':  $model = '&models=1965';
+        break;
+        case 'Nautilus':  $model = '&models=106';
+        break;
+        case 'Neptune':  $model = '&models=2436';
+        break;
+        case 'Pagoda':  $model = '&models=107';
+        break;
+        case 'Perpetual Calendar':  $model = '&models=99';
+        break;
+        case 'Travel Time':  $model = '&models=108';
+        break;
+        case 'Twenty~4':  $model = '&models=109';
+        break;
+        case 'Vintage':  $model = '&models=110';
+        break;
+        case 'World Time':  $model = '&models=95';
+        break;
+    }
+    return $model;
+}
+    ?>
