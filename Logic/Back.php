72af9960178349e4693cea475ea4860aa3801746
NadiiaGoncharenko <83373973+NadiiaGoncharenko@users.noreply.github.com>
2023-01-09 12:09:46 +0100

    orte sind da

--- Logic/Back.php
+++ Logic/Back.php
@@ -44,6 +44,103 @@ if (isset($_SESSION["userID"]) && !empty($_SESSION["userID"])) {
    }
 
    
+switch ($ort) {
+    case 'Ägypten':
+        $ort = 'countryIds=EG';break;
+case 'Andora':$ort = 'countryIds=AD';break;
+case 'Albanien':$ort = 'countryIds=AL';break;
+case 'Armenien':$ort = 'countryIds=AM';break;
+case '':$ort = 'countryIds=AR';break;
+case 'Australien':$ort = 'countryIds=AU';break;
+case 'Bahrain':$ort = 'countryIds=BU';break;
+case 'Bermuda':$ort = 'countryIds=BM';break;
+case 'Belgien':$ort = 'countryIds=BE';break;
+case 'Brasilien':$ort = 'countryIds=BR';break;
+case 'Bulgarien':$ort = 'countryIds=BG';break;
+case 'Chile':$ort = 'countryIds=CL';break;
+case 'China':$ort = 'countryIds=CN';break;
+case 'Costa Rika':$ort = 'countryIds=CR';break;
+case 'Dänemark':$ort = 'countryIds=DK';break;
+case 'Deutschland':$ort = 'countryIds=DE';break;
+case 'Estland':$ort = 'countryIds=EE';break;
+case 'Finnland':$ort = 'countryIds=FI';break;
+case 'Frankreich':$ort = 'countryIds=FR';break;
+case 'Französisch-Polynesien':$ort = 'countryIds=PF';break;
+case 'Georgien':$ort = 'countryIds=GE';break;
+case 'Gibraltar':$ort = 'countryIds=GI';break;
+case 'Griechenland':$ort = 'countryIds=GR';break;
+case 'Großbritannien':$ort = 'countryIds=UK';break;
+case 'Guam':$ort = 'countryIds=GU';break;
+case 'Hongkong':$ort = 'countryIds=HK';break;
+case 'Indien':$ort = 'countryIds=IN';break;
+case 'Indonesien':$ort = 'countryIds=ID';break;
+case 'Irland':$ort = 'countryIds=IE';break;
+case 'Island':$ort = 'countryIds=IS';break;
+case 'Israel':$ort = 'countryIds=IL';break;
+case 'Italien':$ort = 'countryIds=T';break;
+case 'Japan':$ort = 'countryIds=JP';break;
+case 'Kanada':$ort = 'countryIds=CA';break;
+case 'Kasachstan':$ort = 'countryIds=KZ';break;
+case 'Kolumbien':$ort = 'countryIds=CO';break;
+case 'Kroatien':$ort = 'countryIds=HR';break;
+case 'Kuwait':$ort = 'countryIds=KW';break;
+case 'Laos':$ort = 'countryIds=LA';break;
+case 'Lettland':$ort = 'countryIds=LW';break;
+case 'Libanon':$ort = 'countryIds=LB';break;
+case 'Liechtenstein':$ort = 'countryIds=LI';break;
+case 'Litauen':$ort = 'countryIds=LT';break;
+case 'Luxemburg':$ort = 'countryIds=LU';break;
+case 'Macau':$ort = 'countryIds=MO';break;
+case 'Malaysia':$ort = 'countryIds=MY';break;
+case 'Malta':$ort = 'countryIds=MT';break;
+case 'Marokko':$ort = 'countryIds=MA';break;
+
+case 'Monaco':$ort = 'countryIds=MR';break;
+case 'Nepal':$ort = 'countryIds=NP';break;
+case 'Neuseeland':$ort = 'countryIds=NZ';break;
+case 'Niederlande':$ort = 'countryIds=NL';break;
+case 'Nigeria':$ort = 'countryIds=NG';break;
+case 'Norwegen':$ort = 'countryIds=NO';break;
+case 'Oman':$ort = 'countryIds=OM';break;
+case 'Österreich':$ort = 'countryIds=AT';break;
+case 'Pakistan':$ort = 'countryIds=PK';break;
+case 'Panama':$ort = 'countryIds=PA';break;
+case 'Philippinen':$ort = 'countryIds=PH';break;
+case 'Polen':$ort = 'countryIds=PL';break;
+case 'Portugal':$ort = 'countryIds=PT';break;
+case 'Puerto Rico':$ort = 'countryIds=PR'; break;
+case 'Qatar':$ort = 'countryIds=QA';break;
+case 'Reunion':$ort = 'countryIds=RE';break;
+case 'Rumänien':$ort = 'countryIds=RO';break;
+case 'Russland':$ort = 'countryIds=RU';break;
+case 'San Marino':$ort = 'countryIds=SM';break;
+case 'Saudi-Arabien':$ort = 'countryIds=SA';break;
+case 'Schweden':$ort = 'countryIds=SE';break;
+case 'Schweiz':$ort = 'countryIds=CH';break;
+case 'Serbien':$ort = 'countryIds=RS';break;
+case 'Singapur':$ort = 'countryIds=SG';break;
+case 'Slowakei':$ort = 'countryIds=SK';break;
+case 'Slowenien':$ort = 'countryIds=SL';break;
+case 'Spanien':$ort = 'countryIds=ES';break;
+case 'Sri Lanka':$ort = 'countryIds=LK';break;
+case 'Südafrika':$ort = 'countryIds=ZA';break;
+case 'Südkorea':$ort = 'countryIds=KR';break;
+case 'Taiwan':$ort = 'countryIds=TW';break;
+case 'Thailand':$ort = 'countryIds=TH';break;
+case 'Trinidad und Tobago':$ort = 'countryIds=TT';break;
+case 'Tschechien':$ort = 'countryIds=CZ';break;
+case 'Tunesien':$ort = 'countryIds=TN';break;
+case 'Türkei':$ort = 'countryIds=TR';break;
+case 'Ukraine':$ort = 'countryIds=UA';break;
+case 'Ungarn':$ort = 'countryIds=HU';break;
+case 'Uruguay':$ort = 'countryIds=UY';break;
+case 'USA':$ort = 'countryIds=US';break;
+case 'Usbekistan':$ort = 'countryIds=UZ';break; 
+case 'VAE':$ort = 'countryIds=AE';break;
+case 'Venezuela':$ort = 'countryIds=VE';break;
+case 'Vietnam':$ort = 'countryIds=VN';break;
+case 'Zypern':$ort = 'countryIds=CY';break;
+    }
 
 
 
