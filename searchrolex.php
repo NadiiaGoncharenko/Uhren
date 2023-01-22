1dee177f0435b9e0b251830efa2cb1263afc32b3
Nadiia Goncharenko <83373973+NadiiaGoncharenko@users.noreply.github.com>
2023-01-22 14:11:55 +0100

    Update searchrolex.php
    keine Angabe hingefügt

--- searchrolex.php
+++ searchrolex.php
@@ -171,6 +171,7 @@
                       Modell
                     </button>
                     <select name="model" class="dropdown-menu" aria-labelledby="defaultDropdown" style="width: 100%;">
+                        <option class="dropdown-item" value="">Keine Angabe</option>
                       <option class="dropdown-item" value="Air King">Air King</option>
                       <option class="dropdown-item" value="Air King Date">Air King Date</option>
                       <option class="dropdown-item" value="Cellini">Cellini</option>
@@ -236,7 +237,7 @@
                       Preis vom
                     </button>
                    <select name="priceFrom" class="dropdown-menu" aria-labelledby="defaultDropdown">
-                        
+                         <option class="dropdown-item" value="">Keine Angabe</option>
                          <option class="dropdown-item" value="7000">7000€</option>
                           <option class="dropdown-item" value="10000">10000€</option>
                           <option class="dropdown-item" value="16000">16000€</option>
@@ -250,6 +251,7 @@
                       Preis bis
                     </button>
                      <select name="priceTo" class="dropdown-menu" aria-labelledby="defaultDropdown">
+                          <option class="dropdown-item" value="">Keine Angabe</option>
                         <option class="dropdown-item" value="7000">7000€</option>
                         <option class="dropdown-item" value="10000">10000€</option>
                         <option class="dropdown-item" value="16000€">16000€</option>
@@ -264,6 +266,7 @@
                       Standort
                     </button>
                     <select name="ort" class="dropdown-menu" aria-labelledby="defaultDropdown" style="width: 100%;">
+                         <option class="dropdown-item" value="">Keine Angabe</option>
                       <option class="dropdown-item" value="Ägypten">Ägypten</option>
                       <option class="dropdown-item" value="Andora">Andora</option>
                       <option class="dropdown-item" value="Albanien">Albanien</option>
@@ -364,6 +367,7 @@
                       Gehäuse
                     </button>
                     <select name="caseMaterial" class="dropdown-menu" aria-labelledby="defaultDropdown">
+                         <option class="dropdown-item" value="">Keine Angabe</option>
                       <option class="dropdown-item" value="Aluminium">Aluminium</option>
                       <option class="dropdown-item" value="Bronze">Bronze</option>
                       <option class="dropdown-item" value="Carbon">Carbon</option>
